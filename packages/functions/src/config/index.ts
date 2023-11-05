9a9ea4a1dcb5256154781032ab7fe261b93d0ae9
Ru Chern Chong <hello@ruchern.xyz>
2023-11-05 23:22:50 +0800

    Add function to download and update data

---
+++ packages/functions/src/config/index.ts
@@ -0,0 +1,8 @@
+export const BASE_URL: string = `https://singapore-ev-trends.ruchern.xyz`;
+
+export enum FUEL_TYPE {
+  DIESEL = "Diesel",
+  ELECTRIC = "Electric",
+  OTHERS = "Others",
+  PETROL = "Petrol",
+}
---
+++ packages/functions/src/types/index.ts
@@ -0,0 +1,19 @@
+import { FUEL_TYPE } from "../config";
+
+export interface Car {
+  month: string;
+  make: string;
+  fuel_type: FUEL_TYPE | string;
+  number: number;
+  selected?: boolean;
+}
+
+export type Dataset = {
+  label: string;
+  data: number[];
+};
+
+export interface ChartDataset extends Dataset {
+  borderColor: string;
+  checked: boolean;
+}

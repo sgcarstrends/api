5489acd10c441c3df1ea72163663a2594f8adc7b
Ru Chern Chong <hello@ruchern.xyz>
2023-11-14 17:45:49 +0800

    Add car filter by year

--- packages/functions/src/car.ts
+++ packages/functions/src/car.ts
@@ -1,7 +1,21 @@
-import { ApiHandler } from "sst/node/api";
+import { ApiHandler, useQueryParams } from "sst/node/api";
 import { Car } from "@lta-datasets-updater/core/car";
 
-export const list = ApiHandler(async (_evt) => ({
-  statusCode: 200,
-  body: JSON.stringify(await Car.list()),
-}));
+export const list = ApiHandler(async (_evt) => {
+  const params = useQueryParams();
+  const cars = await Car.list();
+
+  const filteredCars =
+    Object.keys(params).length > 0
+      ? cars.filter(({ month }) => {
+          const [year] = month.split("-");
+
+          return year === params.year;
+        })
+      : cars;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(filteredCars),
+  };
+});

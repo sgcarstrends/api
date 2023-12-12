53e7fc8b7cb34646b4b1403315557a1b1bd6a649
Ru Chern Chong <hello@ruchern.xyz>
2023-12-12 23:27:44 +0800

    Fix reading of file from path
    Add better resiliency to find and read the particular file after extracting be it in or outside of a directory

--- packages/core/src/datasets.ts
+++ packages/core/src/datasets.ts
@@ -6,24 +6,36 @@ import * as d3 from "d3";
 import db from "../../config/db";
 
 export const updater = async () => {
-  const tempDir = "/tmp";
+  const extractToPath = "/tmp";
   const zipFileName = `Monthly New Registration of Cars by Make.zip`;
-  const zipFilePath = `${tempDir}/${zipFileName}`;
+  const zipFilePath = `${extractToPath}/${zipFileName}`;
   const csvFileName = `M03-Car_Regn_by_make.csv`;
-  const csvFilePath = `${tempDir}/${csvFileName}`;
+  const csvFilePath = `${extractToPath}/${csvFileName}`;
   const zipUrl = `https://datamall.lta.gov.sg/content/dam/datamall/datasets/Facts_Figures/Vehicle Registration/${zipFileName}`;
 
   const response = await fetch(zipUrl);
   if (!response.ok) {
     throw new Error(`Failed to download the ZIP file: ${response.statusText}`);
   }
-  const data = await response.buffer();
-  fs.writeFileSync(zipFilePath, data);
 
   const zip = new AdmZip(zipFilePath);
-  zip.extractAllTo(`${tempDir}`, true);
+  zip.extractAllTo(`${extractToPath}`, true);
+  const zipEntries = zip.getEntries();
 
-  const csvData = fs.readFileSync(csvFilePath, "utf-8");
+  let destinationPath = extractToPath;
+  zipEntries.forEach((entry) => {
+    if (!entry.isDirectory) {
+      const entryName = entry.entryName;
+
+      destinationPath = `${extractToPath}/${entryName}`;
+
+      const content = entry.getData();
+      fs.writeFileSync(destinationPath, content);
+      zip.deleteFile(entryName);
+    }
+  });
+
+  const csvData = fs.readFileSync(destinationPath, "utf-8");
   const parsedData = d3.csvParse(csvData);
 
   const existingData = await db.collection("cars").find().toArray();

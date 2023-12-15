37f31212552574f419418fb676c4f95279ab2899
Ru Chern Chong <hello@ruchern.xyz>
2023-12-15 15:38:11 +0800

    Clean up type for Car

--- packages/core/src/car.ts
+++ packages/core/src/car.ts
@@ -1,47 +1,52 @@
-import { Document, InferIdType } from "mongodb";
+export * as Car from "./car";
+import { ObjectId } from "mongodb";
 import { format, subMonths } from "date-fns";
 import db from "../../config/db";
 import { FUEL_TYPE } from "./config";
 import type { CarType } from "./types";
 
-export * as Car from "./car";
+// TODO: Will return to clean up the types
+interface CarDocument extends CarType {
+  _id: ObjectId;
+}
 
 const today = new Date();
 const trailingTwelveMonths = format(subMonths(today, 12), "yyyy-MM");
 
 const getCarsByFuelType = async (
   fuelType: FUEL_TYPE,
-): Promise<Document & { _id: InferIdType<Document> }> => {
+): Promise<CarDocument[]> => {
   let cars = await db
-    .collection("cars")
+    .collection<CarDocument>("cars")
     .find({ fuel_type: fuelType, month: { $gte: trailingTwelveMonths } })
     .toArray();
 
-  return cars.reduce((result: any, { _id, month, make, fuel_type, number }) => {
-    const existingCar = result.find(
-      (car: CarType) => car.month === month && car.make === make,
-    );
+  return cars.reduce(
+    (result: CarDocument[], { _id, month, make, fuel_type, number }) => {
+      const existingCar = result.find(
+        (car: CarType) => car.month === month && car.make === make,
+      );
 
-    if (existingCar) {
-      existingCar.number += Number(number);
-    } else {
-      result.push({
-        _id,
-        month,
-        make,
-        fuel_type,
-        number: Number(number),
-      });
-    }
+      if (existingCar) {
+        existingCar.number += Number(number);
+      } else {
+        result.push({
+          _id,
+          month,
+          make,
+          fuel_type,
+          number: Number(number),
+        });
+      }
 
-    return result;
-  });
+      return result;
+    },
+    [],
+  );
 };
 
-export const electric = async (): Promise<
-  Document & { _id: InferIdType<Document> }
-> => getCarsByFuelType(FUEL_TYPE.ELECTRIC);
+export const electric = async (): Promise<CarDocument[]> =>
+  getCarsByFuelType(FUEL_TYPE.ELECTRIC);
 
-export const petrol = async (): Promise<
-  Document & { _id: InferIdType<Document> }
-> => getCarsByFuelType(FUEL_TYPE.PETROL);
+export const petrol = async (): Promise<CarDocument[]> =>
+  getCarsByFuelType(FUEL_TYPE.PETROL);
--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -1,12 +1,12 @@
 import { FUEL_TYPE } from "../config";
 
-export interface CarType {
+export type CarType = {
   month: string;
   make: string;
   fuel_type: FUEL_TYPE | string;
   number: number;
   selected?: boolean;
-}
+};
 
 export type Dataset = {
   label: string;

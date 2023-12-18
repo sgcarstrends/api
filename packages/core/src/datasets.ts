07456f305ea37970887e070149c49444b6decc39
Ru Chern Chong <hello@ruchern.xyz>
2023-12-19 00:00:10 +0800

    Refactor dataset updater

--- packages/core/src/datasets.ts
+++ packages/core/src/datasets.ts
@@ -1,49 +1,40 @@
 export * as Datasets from "./datasets";
-import fs from "fs";
+import fs from "fs/promises";
 import AdmZip from "adm-zip";
 import * as d3 from "d3";
 import { downloadFile } from "./lib/downloadFile";
 import db from "../../config/db";
 
-export const updater = async () => {
-  const extractToPath = "/tmp";
-  const zipFileName = `Monthly New Registration of Cars by Make.zip`;
-  const zipFilePath = `${extractToPath}/${zipFileName}`;
-  const zipUrl = `https://datamall.lta.gov.sg/content/dam/datamall/datasets/Facts_Figures/Vehicle Registration/${zipFileName}`;
+// Constants
+const EXTRACT_PATH: string = "/tmp";
+const ZIP_FILE_NAME: string = `Monthly New Registration of Cars by Make.zip`;
+const ZIP_URL: string = `https://datamall.lta.gov.sg/content/dam/datamall/datasets/Facts_Figures/Vehicle Registration/${ZIP_FILE_NAME}`;
+const COLLECTION_NAME: string = "cars";
 
-  await downloadFile({
-    url: zipUrl,
-    destination: zipFilePath,
-  });
-
+// Helper function to extract ZIP file
+const extractZipFile = async (
+  zipFilePath: string,
+  extractToPath: string,
+): Promise<string> => {
   const zip = new AdmZip(zipFilePath);
-  zip.extractAllTo(`${extractToPath}`, true);
-  const zipEntries = zip.getEntries();
+  zip.extractAllTo(extractToPath, true);
+  const entry = zip.getEntries().find((entry) => !entry.isDirectory);
+  return entry ? entry.entryName : "";
+};
 
-  let destinationPath = extractToPath;
-  zipEntries.forEach((entry) => {
-    if (!entry.isDirectory) {
-      const entryName = entry.entryName;
+export const updater = async (): Promise<{ message: string }> => {
+  try {
+    const zipFilePath = `${EXTRACT_PATH}/${ZIP_FILE_NAME}`;
+    await downloadFile({ url: ZIP_URL, destination: zipFilePath });
 
-      destinationPath = `${extractToPath}/${entryName}`;
+    const extractedFileName = await extractZipFile(zipFilePath, EXTRACT_PATH);
+    const destinationPath = `${EXTRACT_PATH}/${extractedFileName}`;
+    console.log(`Destination path:`, destinationPath);
 
-      const content = entry.getData();
-      fs.writeFileSync(destinationPath, content);
-    }
-  });
+    const csvData = await fs.readFile(destinationPath, "utf-8");
+    const parsedData = d3.csvParse(csvData);
 
-  console.log(destinationPath);
-
-  const csvData = fs.readFileSync(destinationPath, "utf-8");
-  const parsedData = d3.csvParse(csvData);
-
-  let message: string;
-
-  const existingData = await db.collection("cars").find().toArray();
-  if (existingData.length === 0) {
-    const result = await db.collection("cars").insertMany(parsedData);
-    message = `${result.insertedCount} document(s) inserted`;
-  } else {
+    const existingData = await db.collection(COLLECTION_NAME).find().toArray();
     const existingDataMap = new Map(
       existingData.map((item) => [item.month, item]),
     );
@@ -51,14 +42,20 @@ export const updater = async () => {
       (newItem) => !existingDataMap.has(newItem.month),
     );
 
+    let message: string;
     if (newDataToInsert.length > 0) {
-      const result = await db.collection("cars").insertMany(newDataToInsert);
+      const result = await db
+        .collection(COLLECTION_NAME)
+        .insertMany(newDataToInsert);
       message = `${result.insertedCount} document(s) inserted`;
     } else {
       message =
         "No new data to insert. The provided data matches the existing records.";
     }
-  }
 
-  return { message };
+    return { message };
+  } catch (error) {
+    console.error(`An error has occurred:`, error);
+    throw error;
+  }
 };

499ac2da1815729d92cc41e7b429758129baa2d1
Ru Chern Chong <hello@ruchern.xyz>
2024-08-18 02:15:39 +0800

    Add API for list of makes

--- src/v1/routes/cars.ts
+++ src/v1/routes/cars.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import db from "../../config/db";
+import redis from "../../config/redis";
 import { getCarsByFuelType } from "../../lib/getCarsByFuelType";
 import type { Car } from "../../types";
 
@@ -8,6 +9,21 @@ const app = new Hono();
 app.get("/", async (c) => {
   const month = c.req.query("month");
   return c.json(await db.collection<Car>("cars").find({ month }).toArray());
+});
+
+app.get("/makes", async (c) => {
+  const CACHE_KEY = "car_makes";
+  const CACHE_TTL = 3600;
+
+  let makes = await redis.get(CACHE_KEY);
+  if (!makes) {
+    makes = await db.collection<Car>("cars").distinct("make");
+    await redis.set(CACHE_KEY, makes, { ex: CACHE_TTL });
+  }
+
+  makes.sort();
+
+  return c.json(makes as string);
 });
 
 app.get("/:fuelType", async (c) => {

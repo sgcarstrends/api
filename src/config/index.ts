963c3d79c7b0613576c0de5bd5a93020149f82f9
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-12-26 00:58:55 +0800

    Add comment to hybrid regex
    To explain the purpose of the regex

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,22 @@ import { FuelType } from "@/types";
 
 export const CACHE_TTL = 24 * 60 * 60;
 
+/**
+ * Regular expression to match strings in the following formats:
+ * - Diesel-Electric
+ * - Diesel-Electric (Plug-In)
+ * - Petrol-Electric
+ * - Petrol-Electric (Plug-In)
+ *
+ * The pattern breakdown is as follows:
+ * - `^` asserts the start of the string.
+ * - `(${FuelType.Diesel}|${FuelType.Petrol})` matches either "Diesel" or "Petrol".
+ * - `-` matches the hyphen character literally.
+ * - `${FuelType.Electric}` matches the string "Electric".
+ * - `(\\s\\(Plug-In\\))?` matches an optional space followed by "(Plug-In)".
+ * - `$` asserts the end of the string.
+ * - `"i"` makes the regular expression case-insensitive.
+ */
 export const HYBRID_REGEX = new RegExp(
   `^(${FuelType.Diesel}|${FuelType.Petrol})-${FuelType.Electric}(\\s\\(Plug-In\\))?$`,
   "i",

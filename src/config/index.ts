450eeab56ef2ee8d900a3bcb9dd49ff31de4624c
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-12-26 00:54:20 +0800

    Fix regex

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ import { FuelType } from "@/types";
 export const CACHE_TTL = 24 * 60 * 60;
 
 export const HYBRID_REGEX = new RegExp(
-  `^(${FuelType.Diesel}|${FuelType.Petrol})-${FuelType.Electric}(\s\(Plug-In\))?$`,
+  `^(${FuelType.Diesel}|${FuelType.Petrol})-${FuelType.Electric}(\\s\\(Plug-In\\))?$`,
   "i",
 );
